cc5e27f156ad341503e923bc8deda968383826cb
a.o <drealayodeji@icloud.com>
2022-02-09 02:57:34 +0000

    Updated with Gitro

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,9 @@ export default class Contact extends Component {
 
   render() {
     return (
-    <div>
+    <div className='overflow-x-hidden'>
         <Nav />
-        <div className='flex flex-col w-screen h-[80vh] justify-center items-center'>
+        <div className='flex flex-col w-screen h-[80vh] justify-center items-center '>
             <img src={qr} className='w-64' alt='WhatsApp QR Code'/>
             <div className='text-center p-7 text-gray-400 text-sm font-light'>Contact us quickly via WhatsApp or via the phone using any of the links below or by scanning the QR code above.</div>
         <a href='https://wa.link/ay9yic' target='_blank' rel='noreferrer' className='text-green-600 bg-gray-800 p-4 px-6 rounded-3xl hover:bg-white'><i class='fa-brands fa-whatsapp px-2'></i>
